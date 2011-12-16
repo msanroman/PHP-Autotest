a05cf0a92108035aabd58c30ae44e591bbdcd601
Miguel San Román <msanromanv@gmail.com>
2011-12-16 23:25:28 +0100

    First commit

---
+++ PHPAutotest.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+allTestsOkIcon=/usr/share/icons/gnome/48x48/status/weather-clear.png
+allTestsOkTitle="All tests passed"
+allTestsOkMessage="Arr! Ye rock mate!"
+
+someTestsFailedIcon=/usr/share/icons/gnome/48x48/status/weather-showers.png
+someTestsFailedTitle="Some tests failed"
+someTestsFailedMessage="Avast! Ye broke sumthin'!"
+
+folderBeingWatched=$1
+phpunitConfigPath=$2
+
+if [ ! -z $folderBeingWatched ]; then
+
+	if [ -z $phpunitConfigPath ]; then
+		phpunitConfigPath=app/
+	fi
+
+	while [ true ]; do
+		inotifywait -qq -e modify $folderBeingWatched -r
+		if phpunit -c $phpunitConfigPath | tail -n3 | grep "Ok"; then
+			notify-send --hint=int:transient:1 --icon=$allTestsOkIcon "$allTestsOkTitle" "$allTestsOkMessage" --expire-time=1500
+		else
+			notify-send --hint=int:transient:1 --icon=$someTestsFailedIcon "$someTestsFailedTitle" "$someTestsFailedMessage" --expire-time=1500
+		fi
+	done
+else
+	echo "USAGE: PHPAutotest folderToLookForModifications [phpUnitConfigPath]"
+fi
---
+++ README.md
@@ -0,0 +1,21 @@
+# **P**irate **H**yper **P**ro countinous test runner (Yarr!)
+
+So far, it works on Fedora 16, and I suppose that all the bunch of Linux distributions that uses NotifyOSD.
+
+## Dependencies:
+
+* inotify-tools
+
+    In order to install it simply run: sudo yum install inotify-tools
+    
+### Usage:
+
+Locate PHPAutotest.sh in your project's root and run it in background, passing as argument the folder you want to _listen_ for changes in order to start a test run when some of its files are modified.
+
+* Sample:
+
+> $sh PHPAutotest.sh src
+
+Also, you can pass as a second argument the folder where you want to retrieve your phpunit config file (by default it expects it to be in _app/_):
+
+> $sh PHPAutotest.sh src app/
